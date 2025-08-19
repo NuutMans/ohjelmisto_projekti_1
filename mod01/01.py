27253a18f83397047ca2bc8699a4dbc120da4ad1
NuutMans <nuuttim@metropolia.fi>
2025-08-19 14:42:57 +0300

    Lisäsin Rivi 7 Nuutti -> NuuttiM

---
+++ mod01/01.py
@@ -0,0 +1,12 @@
+# Tässä on kommentti.
+print('toimiikos tämä!?!?!?')
+print('Hei Nuutti Manssila')
+print ('"Moi" \n-sanoi Nuutti')
+
+# yksinkertainen muuttuja
+nimi = 'NuuttiM'
+print(nimi)
+
+# syöte, eli tietoa käyttäjältä
+nimi2 = input('Moikka, anna nimesi: ')
+print(nimi2)
