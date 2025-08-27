82d867121446cdcca970ac0659a8ff1fa473fcdf
NuutMans <nuuttim@metropolia.fi>
2025-08-27 11:23:51 +0300

    apua taululta

---
+++ mod04/04_tehtavat.py
@@ -0,0 +1,18 @@
+# mod04 tehtävä 6
+import random
+# Kaikkien pisteiden määrä N
+N = 10 # kysy käyttäjän laittamaan arvon.
+# ympyrän sisään osuvien pisteiden lkm n
+i = 0
+while i > N:
+        i = i + 1
+        # arvotaan random/satunnainen piste koordinaatistoon (x, y)
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        print(f"Arvottu piste: {x}, {y}")
+
+        # todo: testaa, toteuttaako piste epäyhtälön x^2+y^2<1
+        # jos ehto on totta, kasvata n-laskurin arvoa  --käytä if-lausetta
+
+
+    #todo: laske ja tulosta piin likiarvo käyttäen kaavaa: π≈4n/N
