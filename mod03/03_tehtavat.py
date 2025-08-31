e1e61db0b9b688cce59a14d0a9e26626e1c68b14
NuutMans <nuuttim@metropolia.fi>
2025-08-31 22:57:24 +0300

    add comments

--- mod03/03_tehtavat.py
+++ mod03/03_tehtavat.py
@@ -18,12 +18,14 @@ if kuhan_pituus < 42:
 # Tehtävä 2, kysyy laivan hyttiluokan (LUX, A, B, C), sen jälkeen tulostaa sen sanallisen kuvauksen.
 
 
-laivan_hytti = input("Kirjoita tähän haluamasi laivan hytti (LUX, A, B, C): ")
+laivan_hytti = input("Kirjoita tähän haluamasi laivan hytti (LUX, A, B, C): ") # kysyy minkä hytin haluaa valita
 LUX = "LUX"
 A = "A"
 B = "B"
 C = "C"
 
+
+# tulostaa laivan_hytti muuttujaan kirjoitetun vastauksen mukaisen tiedotteen
 if laivan_hytti == LUX:
     print("LUX on parvekkeellinen hytti yläkannella.")
 elif laivan_hytti == A:
@@ -38,20 +40,20 @@ elif laivan_hytti == C:
 # Tehtävä 3, Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
 # Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
 
-sukupuoli = input("Kirjoita sukupuoli (nainen tai mies): ")
+sukupuoli = input("Kirjoita sukupuoli (nainen tai mies): ") # kysytään sukupuolen muuttuja
 
-hemoglobiini = float(input("Kirjoita hemoglobiiniarvo: "))
+hemoglobiini = float(input("Kirjoita hemoglobiiniarvo: ")) # kysytään hemoglobiinin arvo
 
 if sukupuoli == "nainen" and hemoglobiini >= 117 and hemoglobiini <= 175:
-    print("Hemoglobiiniarvosi on normaali")
+    print("Hemoglobiiniarvosi on normaali") # jos sukupuoli on nainen ja hemoglobiinin arvo on 117 ja 175 välillä, tulostaa normaali
 elif sukupuoli == "nainen" and hemoglobiini < 117:
-    print("Hemoglobiiniarvosi on alhainen")
+    print("Hemoglobiiniarvosi on alhainen") # jos sukupuoli on nainen ja hemoglobiinin arvo on alle 117, tulostaa alhainen
 elif sukupuoli == "nainen" and hemoglobiini > 175:
-    print("Hemoglobiiniarvosi on korkea")
+    print("Hemoglobiiniarvosi on korkea") # jos sukupuoli on nainen ja hemoglobiinin arvo on yli 175, tulostaa korkea
 
 if sukupuoli == "mies" and hemoglobiini >= 134 and hemoglobiini <= 195:
-    print("Hemoglobiiniarvosi on normaali")
+    print("Hemoglobiiniarvosi on normaali")  # jos sukupuoli on mies ja hemoglobiinin arvo on 134 ja 195 välillä, tulostaa normaali
 elif sukupuoli == "mies" and hemoglobiini < 134:
-    print("Hemoglobiiniarvosi on alhainen")
+    print("Hemoglobiiniarvosi on alhainen") # jos sukupuoli on mies ja hemoglobiinin arvo on alle 134, tulostaa alhainen
 elif sukupuoli == "mies" and hemoglobiini > 195:
-    print("Hemoglobiiniarvosi on korkea")
+    print("Hemoglobiiniarvosi on korkea") # jos sukupuoli on mies ja hemoglobiinin arvo on yli 195, tulostaa korkea
