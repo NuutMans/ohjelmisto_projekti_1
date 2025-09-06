95e8bbf728d4247a32e36fc28191a0f63e647633
NuutMans <nuuttim@metropolia.fi>
2025-09-06 23:51:13 +0300

    done

--- mod04/04_tehtava_5.py
+++ mod04/04_tehtava_5.py
@@ -1,34 +1,24 @@
 # mod04 tehtävä 5
-'''
-
-Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen (python) ja salasanan (rules)
-Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen
-Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa
-Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty
-
-'''
-
-
-
 
 oikea_salasana = "rules"
 oikea_tunnus = "python"
 yritykset = 5
 
-
-while yritykset > 0:
+# kirjautumislooppi, joka kysyy kirjautumista 5 kertaa, jonka jälkeen lopettaa ohjelman, jos kirjautumiset on väärin
+while yritykset > -1:
 
     input_tunnus = input("Käyttäjätunnus: ")
-    input_salasana = input("Kirjoita minulle salasanasi: ")
+    input_salasana = input("Salasana: ")
     yritykset -= 1
 
-    if input_salasana == oikea_salasana and input_tunnus == oikea_tunnus and yritykset > 0:
+
+    if input_salasana == oikea_salasana and input_tunnus == oikea_tunnus and yritykset > -1:
         print ("Tervetuloa!")
         break
-
-    elif input_salasana != oikea_salasana or input_tunnus != oikea_tunnus and yritykset > 0:
-            print(f"Väärä salasana! Yrityksiä jäljellä: {yritykset}")
+    elif input_salasana != oikea_salasana or input_tunnus != oikea_tunnus and yritykset > -1:
+        print(f"Väärä salasana! Yrityksiä jäljellä: {yritykset}")
 
     elif yritykset <= 0:
         print("Salasana meni liian monta kertaa väärin. Ohjelma päättyy...")
+
         break
