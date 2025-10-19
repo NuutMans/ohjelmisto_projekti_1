b8ae9246cf8e0b068d3b0719da40e56e9cfe6d78
NuutMans <nuuttim@metropolia.fi>
2025-10-19 12:53:25 +0300

    done

---
+++ mod06/06_tehtava_2.py
@@ -0,0 +1,21 @@
+
+
+
+import random
+
+
+def number(sides):
+    return random.randint(1, sides)
+
+
+sides = int(input("Anna nopan sivujen määrä: "))
+
+app_running = True
+while app_running:
+    result = number(sides)
+    print (result)
+    if result == sides:
+        app_running = False
+
+
+
