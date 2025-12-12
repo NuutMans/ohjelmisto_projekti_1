7552e9b14e75d8f530af8180bc98ffc9466d8635
NuutMans <nuuttim@metropolia.fi>
2025-12-13 00:50:08 +0200

    done

--- mod09/09_tehtava_1.py
+++ mod09/09_tehtava_1.py
@@ -1,20 +1,35 @@
-class Car():
+class Car:
     def __init__(self, register_number, top_speed):
-        print ("New car has been created")
+        print("New car has been created")
         self.register_number = register_number
         self.top_speed = top_speed
-        #todo anna huippunopeus ja rekkari alustajan parametreina ✅
         self.speed = 0
         self.trip = 0
-    def accelerate(self, speed):
-        # todo anna nopeuden muutos parametreina ja huomioi huippunopeus
-        # todo auton nopeus ei saa mennä negatiiviseksi, if-else -rakenteita joutuu käyttämään
-        self.speed = speed
 
-#hyödynnetään listoja, ei ole järkeä käyttää vaikka sataan autoon car1, car2, car3
+    def accelerate(self, speed_change):
+        # Muutetaan nopeutta
+        self.speed += speed_change
+
+        # Huippunopeus
+        if self.speed > self.top_speed:
+            self.speed = self.top_speed
+
+        # Nopeus ei saa olla negatiivinen
+        if self.speed < 0:
+            self.speed = 0
 
 
-car1 = Car("ABC-123",142)
+# Ohjelma
+car1 = Car("ABC-123", 142)
+
 car1.accelerate(30)
 car1.accelerate(70)
-print(f"Auton 1 \n nopeus: {car1.top_speed} \n rekkari: {car1.register_number} \n nopeus: {car1.speed} \n matka: {car1.trip}")
+car1.accelerate(-50)
+
+print(
+    f"Auton 1 tiedot:\n"
+    f"Rekkari: {car1.register_number}\n"
+    f"Huippunopeus: {car1.top_speed} km/h\n"
+    f"Nopeus: {car1.speed} km/h\n"
+    f"Matka: {car1.trip} km"
+)
--- mod09/09_tehtava_2.py
+++ mod09/09_tehtava_2.py
@@ -1,22 +1,27 @@
-class Car():
-    def __init__(self, register_number, top_speed, accelerate):
-        print ("New car has been created")
-        self.register_number = register_number
-        self.top_speed = top_speed
-        #todo anna huippunopeus ja rekkari alustajan parametreina
-        self.speed = 0
-        self.trip = 0
-    def accelerate(self):
-        # todo anna nopeuden muutos parametreina ja huomioi huippunopeus
-        # todo auton nopeus ei saa mennä negatiiviseksi, if-else -rakenteita joutuu käyttämään
-        self.speed += 5
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.kuljettu_matka = 0
 
-#hyödynnetään listoja, ei ole järkeä käyttää vaikka sataan autoon car1, car2, car3
+    def kiihdyta(self, nopeuden_muutos):
+        self.nopeus += nopeuden_muutos
+
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = self.huippunopeus
+        elif self.nopeus < 0:
+            self.nopeus = 0
 
 
-car1 = Car("ABC-123",142)
-car1.accelerate()
-car1.accelerate()
-car1.accelerate()
+# Ohjelma
+auto = Auto("ABC-123", 142)
 
-print(f"Auton 1 nopeus: {car1.top_speed}, rekkari: {car1.register_number}, nopeus: {car1.speed}, matka: {car1.trip}")
+auto.kiihdyta(30)
+auto.kiihdyta(70)
+auto.kiihdyta(50)
+
+print("Auton nopeus kiihdytysten jälkeen:", auto.nopeus, "km/h")
+
+auto.kiihdyta(-200)
+print("Auton nopeus hätäjarrutuksen jälkeen:", auto.nopeus, "km/h")
